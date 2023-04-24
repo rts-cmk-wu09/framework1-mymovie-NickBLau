9a8ea624f9feac82adb89a203d7fb8592f594b8c
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-04-24 09:15:48 +0200

    Add image and box shadow

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,7 +1,11 @@
-import coverimage from "../assets/Poster.png";
+import styled from "styled-components";
+import coverimage from "../assets/poster.png";
+
+const StyledImg = styled.img`
+  box-shadow: 0px 5px 10px rgb(0 0 0 / 0.5);
+`;
 
 const Image = () => {
-  return <img src={coverimage} alt="Movie poster" />;
+  return <StyledImg src={coverimage} alt="blablabla" />;
 };
-
 export default Image;
