6f36ba471d364d937772cfdb95edc33185ed9a5b
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-05-08 10:03:20 +0200

    Add api to NowShowing

--- src/templates/NowShowing.jsx
+++ src/templates/NowShowing.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Image from "../components/Image";
 import MovieRating from "../components/MovieRating";
 import Heading from "../components/Heading";
+import { useLoaderData } from "react-router-dom";
 
 const StyledArticle = styled.article`
   height: 283px;
@@ -11,6 +12,7 @@ const StyledArticle = styled.article`
 `;
 
 const NowShowing = () => {
+  const MovieCardData = useLoaderData();
   return (
     <StyledArticle>
       <figure>
@@ -21,5 +23,13 @@ const NowShowing = () => {
     </StyledArticle>
   );
 };
+
+export async function loader() {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/movie/now_playing/?api_key="
+  );
+  const data = await res.json();
+  return data;
+}
 
 export default NowShowing;
