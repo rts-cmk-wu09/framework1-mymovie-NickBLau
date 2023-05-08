3e976caeffd77025fc9d25af13d344ac99decf4b
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-05-08 09:50:21 +0200

    Changed Heading font weight

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 const StyledHeading = styled((props) => props.as)`
   font-size: ${(props) => props.size}px;
+  font-weight: ${(props) => props.weight};
+  font-family: ${(props) => props.family};
   grid-column-start: 2;
   justify-self: center;
 `;
