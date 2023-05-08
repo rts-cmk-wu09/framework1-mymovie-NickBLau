560964ea361dc97e53b66ba74d44e0902f54bc79
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-05-08 09:50:50 +0200

    Changed Main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import DetailView from "./Pages/DetailsView";
+import ListView from "./Pages/ListView";
+import ErrorView from "./Pages/ErrorView";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import App from "./App";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} errorElement={<ErrorView />}>
+      <Route index element={<ListView />} />
+      <Route path="/details/:id" element={<DetailView />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
