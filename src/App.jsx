6e6432ff84ac0c987ed43734f536b2ad86c10adc
NickBLau <114568331+NickBLau@users.noreply.github.com>
2023-05-08 09:52:09 +0200

    Changed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,13 @@
 import "./App.css";
-import MovieCard from "./templates/MovieCard";
-import PopularCard from "./templates/PopularCard";
-import SeeMoreButton from "./components/SeeMoreButton";
-import MovieGenreButton from "./components/MovieGenreButton";
-import Heading from "./components/Heading";
-import Switch from "./components/Switch";
-import Footer from "./components/footer";
+import { Outlet } from "react-router-dom";
+// import DetailsView from "./Pages/DetailsView";
+// import ListView from "./Pages/ListView";
 
 function App() {
   return (
     <div className="App">
-      <header className="gridContainer">
-        <Heading title="MyMovies" size="16" as="h1" />
-        <Switch />
-      </header>
-      <main>
-        <section>
-          <div className="flexContainer space-between">
-            <Heading title="Now Showing" size="16" as="h2" />
-            <SeeMoreButton />
-          </div>
-          <div className="flexContainer movieCardContainer">
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-          </div>
-        </section>
-        <section>
-          <div className="flexContainer space-between">
-            <Heading title="Popular" size="16" as="h2" />
-            <SeeMoreButton />
-          </div>
-          <PopularCard />
-          <PopularCard />
-          <PopularCard />
-          <PopularCard />
-          <PopularCard />
-          <PopularCard />
-        </section>
-      </main>
-      <Footer />
+      <Outlet />
     </div>
   );
 }
-
 export default App;
